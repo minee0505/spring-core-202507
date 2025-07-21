ba36fbc718a84268acea2539ba2fcc6b21acd0a3
minee0505 <wodeyue97@gmail.com>
2025-07-21 16:27:23 +0900

    Add core classes for basic application flow in chap02_3 package
    - Implement `MyRepository` for database CRUD operations
    - Create `MyService` class to handle data processing
    - Add `MyController` for client request handling
    - Introduce `MyModel` as a simple data object
    - Support constructor-based dependency injection across components

---
+++ src/main/java/com/spring/core/chap02_3/controller/MyController.java
@@ -0,0 +1,20 @@
+package com.spring.core.chap02_3.controller;
+
+import com.spring.core.chap02_3.service.MyService;
+
+// 클라이언트의 요청을 처리하는 클래스
+public class MyController {
+
+    private MyService service;
+
+    public MyController(MyService service) {
+        System.out.println("MyController 객체 생성!");
+        this.service = service;
+        System.out.println("service 의존성이 controller에 주입됨!");
+    }
+
+    // 정제된 데이터를 응답해주세요!
+    public void handleRequest() {
+        System.out.println("정제된 데이터를 응답 -> " + service.processData());
+    }
+}
